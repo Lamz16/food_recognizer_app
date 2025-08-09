b2a1e0c718fd81b0dd7be3c29d5ef88e94055c71
Lamz16 <andiku0755@gmail.com>
2025-08-09 19:36:05 +0700

    Solved unreference import after change name of project

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:submission/ui/result_page.dart';
+
+import '../ui/result_page.dart';
 
 class HomeController extends ChangeNotifier {
   void goToResultPage(BuildContext context) {
