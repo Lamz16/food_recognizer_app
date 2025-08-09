243e3f11cbf4cf8af497b2f077f32aec4fbb4122
Lamz16 <andiku0755@gmail.com>
2025-08-09 20:10:23 +0700

    add set image path on controller

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 import '../ui/result_page.dart';
 
 class HomeController extends ChangeNotifier {
+
+  String? selectedImagePath;
+
+  void setImage(String path) {
+    selectedImagePath = path;
+    notifyListeners();
+  }
+
   void goToResultPage(BuildContext context) {
     Navigator.push(
       context,
